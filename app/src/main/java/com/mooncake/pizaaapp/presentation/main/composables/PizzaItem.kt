ddfa9593276cf599bec12e150382698c11c17f58
Mohammed Sayed <83292287+Mohammedbih@users.noreply.github.com>
2023-07-11 21:58:02 +0300

    refactor PizzaItem

--- app/src/main/java/com/mooncake/pizaaapp/presentation/main/composables/PizzaItem.kt
+++ app/src/main/java/com/mooncake/pizaaapp/presentation/main/composables/PizzaItem.kt
@@ -1,21 +1,26 @@
 package com.mooncake.pizaaapp.presentation.main.composables
 
-import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.FiniteAnimationSpec
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.spring
+import androidx.compose.animation.fadeOut
 import androidx.compose.animation.scaleIn
-import androidx.compose.animation.shrinkOut
-import androidx.compose.animation.with
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
@@ -31,7 +36,11 @@ import com.mooncake.pizaaapp.presentation.main.Pizza
 @Composable
 fun PizzaItem(
     pizza: Pizza,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    spec: FiniteAnimationSpec<Float> = spring(
+        dampingRatio = Spring.DampingRatioHighBouncy,
+        stiffness = Spring.StiffnessMedium
+    )
 ) {
     val screenWidth = LocalConfiguration.current.screenWidthDp
     val scale by animateFloatAsState(
@@ -40,10 +49,7 @@ fun PizzaItem(
             Pizza.PizzaSize.MEDIUM -> 0.85f
             Pizza.PizzaSize.LARGE -> 1f
         },
-        animationSpec = spring(
-            dampingRatio = Spring.DampingRatioMediumBouncy,
-            stiffness = Spring.StiffnessMedium
-        )
+        animationSpec = spec
     )
 
     Box(
@@ -54,18 +60,23 @@ fun PizzaItem(
             painter = painterResource(id = pizza.image),
             contentDescription = "pizza",
             contentScale = ContentScale.Crop,
-            modifier = Modifier.size((screenWidth * 0.52).dp).scale(scale)
+            modifier = Modifier
+                .size((screenWidth * 0.52).dp)
+                .scale(scale)
         )
-        pizza.ingredients.forEach { ingredient ->
-            AnimatedContent(
-                targetState = ingredient,
-                transitionSpec = { scaleIn() with shrinkOut() }
+        Pizza.PizzaIngredient.values().forEach { ingredient ->
+            AnimatedVisibility(
+                visible = pizza.ingredients.contains(ingredient),
+                enter = scaleIn(initialScale = 4f, animationSpec = spec),
+                exit = fadeOut(animationSpec = spec)
             ) {
                 Image(
-                    painter = painterResource(id = it.image),
+                    painter = painterResource(id = ingredient.image),
                     contentDescription = "ingredient",
                     contentScale = ContentScale.Crop,
-                    modifier = Modifier.size((screenWidth * 0.4).dp).scale(scale)
+                    modifier = Modifier
+                        .size((screenWidth * 0.4).dp)
+                        .scale(scale)
                 )
             }
         }
@@ -75,13 +86,9 @@ fun PizzaItem(
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 private fun Preview() {
-    Column(
-        Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        PizzaItem(
-            pizza = Pizza(
+    var pizza by remember {
+        mutableStateOf(
+            Pizza(
                 id = 0,
                 R.drawable.bread_1,
                 prices = mapOf(
@@ -90,41 +97,25 @@ private fun Preview() {
                     Pizza.PizzaSize.LARGE to 30
                 ),
                 size = Pizza.PizzaSize.LARGE,
-                ingredients = setOf(
-                    Pizza.PizzaIngredient.BASIL,
-                    Pizza.PizzaIngredient.MUSHROOM,
-                )
             )
         )
-        PizzaItem(
-            pizza = Pizza(
-                id = 0,
-                R.drawable.bread_4,
-                prices = mapOf(
-                    Pizza.PizzaSize.SMALL to 10,
-                    Pizza.PizzaSize.MEDIUM to 15,
-                    Pizza.PizzaSize.LARGE to 30
-                ),
-                size = Pizza.PizzaSize.MEDIUM,
-                ingredients = setOf(
-                    Pizza.PizzaIngredient.BASIL,
-                    Pizza.PizzaIngredient.BROCCOLI,
-                    Pizza.PizzaIngredient.ONION,
-                    Pizza.PizzaIngredient.SAUSAGE,
-                )
+    }
+
+    Column(
+        Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        PizzaItem(pizza = pizza)
+        Button(onClick = { pizza = pizza.copy(size = Pizza.PizzaSize.values().random()) }) {
+            Text(text = "scale")
+        }
+        Button(onClick = {
+            pizza = pizza.copy(
+                ingredients = pizza.ingredients + Pizza.PizzaIngredient.values().random()
             )
-        )
-        PizzaItem(
-            pizza = Pizza(
-                id = 0,
-                R.drawable.bread_2,
-                prices = mapOf(
-                    Pizza.PizzaSize.SMALL to 10,
-                    Pizza.PizzaSize.MEDIUM to 15,
-                    Pizza.PizzaSize.LARGE to 30
-                ),
-                size = Pizza.PizzaSize.SMALL
-            )
-        )
+        }) {
+            Text(text = "ingredients")
+        }
     }
 }
