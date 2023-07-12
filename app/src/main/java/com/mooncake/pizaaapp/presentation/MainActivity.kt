7e72b5c1faeb78002c5340466a610e40d2225251
Mohammed Sayed <83292287+Mohammedbih@users.noreply.github.com>
2023-07-12 03:02:55 +0300

    optimize imports

--- app/src/main/java/com/mooncake/pizaaapp/presentation/MainActivity.kt
+++ app/src/main/java/com/mooncake/pizaaapp/presentation/MainActivity.kt
@@ -4,14 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.mooncake.pizaaapp.presentation.main.MainScreen
 import com.mooncake.pizaaapp.presentation.main.MainViewModel
 import com.mooncake.pizaaapp.presentation.ui.theme.PizaaAppTheme
