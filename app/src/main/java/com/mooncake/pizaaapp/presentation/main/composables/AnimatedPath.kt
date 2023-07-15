bf29824c7c5881d2cb3556d37a6d953e162f3711
Mohammed Sayed <83292287+Mohammedbih@users.noreply.github.com>
2023-07-15 16:56:39 +0300

    test animated path

--- app/src/main/java/com/mooncake/pizaaapp/presentation/main/composables/AnimatedPath.kt
+++ app/src/main/java/com/mooncake/pizaaapp/presentation/main/composables/AnimatedPath.kt
@@ -1,64 +1,87 @@
 package com.mooncake.pizaaapp.presentation.main.composables
 
 import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.PathMeasure
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun AnimatedPath(
-
-) {
-    val drawPathAnimation = remember {
-        Animatable(0f)
-    }
-    val pathMeasure = remember {
-        PathMeasure()
-    }
-    val fullGraphPath = remember {
-        mutableStateOf(Path().apply {
-            moveTo(0f,0f)
-            lineTo(500f,0f)
-            lineTo(500f,500f)
-            lineTo(0f,500f)
-        })
-    }
-    LaunchedEffect(key1 = Unit, block = {
-        drawPathAnimation.animateTo(
-            1f,
-            animationSpec = tween(10000)
-        )
-    })
+fun AnimatedPath() {
+    val drawPathAnimation = remember { Animatable(0f) }
+    val pathMeasure = remember { PathMeasure() }
+    var fullGraphPath by remember { mutableStateOf(Path()) }
     val animatedPath = remember {
         derivedStateOf {
             val newPath = Path()
-            pathMeasure.setPath(fullGraphPath.value, false)
+            pathMeasure.setPath(fullGraphPath, false)
             pathMeasure.getSegment(
                 0f,
                 drawPathAnimation.value * pathMeasure.length,
                 newPath,
-                false
+                true
             )
             newPath
         }
     }
-    Canvas(modifier = Modifier.size(200.dp)) {
-        drawPath(path = animatedPath.value, color = Color.Cyan, style = Stroke(width = 4.dp.toPx()))
+
+    Canvas(modifier = Modifier.size(400.dp)) {
+        fullGraphPath = getHeartPath()
+        drawPath(
+            path = animatedPath.value,
+            color = Color.Black,
+            style = Stroke(width = 4.dp.toPx(), cap = StrokeCap.Round)
+        )
+    }
+
+    LaunchedEffect(key1 = Unit) {
+        drawPathAnimation.animateTo(
+            1f,
+            animationSpec = tween(10000, easing = LinearEasing)
+        )
+    }
+}
+
+private fun DrawScope.getHeartPath(): Path {
+    return Path().apply {
+        moveTo(size.width * 0.5f, size.height * 0.9f)
+        cubicTo(
+            size.width * -0.2f,
+            size.height * 0f,
+            size.width * 0.5f,
+            size.height * 0.1f,
+            size.width * 0.5f,
+            size.height * 0.4f,
+        )
+        cubicTo(
+            size.width * 0.5f,
+            size.height * 0.1f,
+            size.width * 1.2f,
+            size.height * 0f,
+            size.width * 0.5f,
+            size.height * 0.9f,
+        )
+        close()
     }
 }
 
@@ -67,7 +90,9 @@ fun AnimatedPath(
 @Composable
 fun PreviewAnimatedPath() {
     Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        AnimatedPath()
+        Box(modifier = Modifier.border(width = 10.dp, color = Color.Green)){
+            AnimatedPath()
+        }
     }
 }
 
